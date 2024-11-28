d7efc64fe03875cc9f9460a478cdb472e172ee71
TolerantLescnq7 <moretolerant0@gmail.com>
2024-11-28 15:33:08 +0800

    revert revert

--- app/src/main/java/com/aqc/adaptikons/services/ClockWidget.java
+++
@@ -1,5 +0,0 @@
-package com.aqc.adaptikons.services;
-
-import candybar.lib.services.CandyBarWidgetService;
-
-public class ClockWidget extends CandyBarWidgetService {}}
--- app/src/main/java/com/aqc/adaptikons/services/MuzeiService.java
+++
@@ -1,5 +0,0 @@
-package com.aqc.adaptikons.services;
-
-import candybar.lib.services.CandyBarMuzeiService;
-
-public class MuzeiService extends CandyBarMuzeiService {}}
